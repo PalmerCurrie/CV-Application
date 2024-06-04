d173e51af6caaa705ad0c3e13e91dd6a1f25f0db
Palmer Currie <pcurri01@students.cs.ubc.ca>
2024-06-03 20:16:39 -0700

    Added section for resume on page

---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+import '../styles/Resume.css'; // Import the CSS file
+
+export default function Resume() {
+
+    return (
+        <div className="container">
+           
+        </div>
+
+    );
+
+
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PersonalDetails from './components/PersonalDetails.jsx'
+// import PersonalDetails from './components/PersonalDetails.jsx'
 import PersonalDetailsInputForm from "./components/PersonalDetailsInputForm.jsx"
+import Resume from './components/Resume.jsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PersonalDetails firstName="first name" />
+    {/* <PersonalDetails firstName="first name" /> */}
     <PersonalDetailsInputForm />
+    <Resume />
   </React.StrictMode>,
 )
